4c10569f9d182d443d7048ac8710e3b9d1f05703
internetms52 <internetms52@gmail.com>
2024-08-08 21:54:04 +0800

    加入RobotTextConfigProviderFetchException的註解
    加入RobotTextConfigProviderFetchException的註解

--- api/src/main/java/hobby/internetms52/robotxtgen/exception/RobotTextConfigProviderFetchException.java
+++ api/src/main/java/hobby/internetms52/robotxtgen/exception/RobotTextConfigProviderFetchException.java
@@ -1,5 +1,13 @@
 package hobby.internetms52.robotxtgen.exception;
 
+/**
+ * Exception thrown when there is a failure in fetching or initializing a RobotTextConfigProvider.
+ * This exception is typically thrown in two scenarios:
+ * 1. When the constructor for the RobotTextConfigProvider cannot be found.
+ * 2. When the creation of a new instance of RobotTextConfigProvider fails.
+ *
+ * @see hobby.internetms52.robotxtgen.config.RobotTextConfigProvider
+ */
 public class RobotTextConfigProviderFetchException extends Exception {
     public RobotTextConfigProviderFetchException(Throwable cause) {
         super(cause);
