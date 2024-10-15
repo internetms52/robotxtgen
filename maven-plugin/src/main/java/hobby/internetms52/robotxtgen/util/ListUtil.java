3ccd7766dc042318edacfc714c30d703a0f4c5d0
internetms52 <internetms52@gmail.com>
2024-10-15 10:42:55 +0800

    add java doc
    add java doc

--- maven-plugin/src/main/java/hobby/internetms52/robotxtgen/util/ListUtil.java
+++ maven-plugin/src/main/java/hobby/internetms52/robotxtgen/util/ListUtil.java
@@ -4,7 +4,16 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+/**
+ * Utility class for List operations.
+ */
 public class ListUtil {
+    /**
+     * Filters out null elements from the input List and retains only String elements.
+     *
+     * @param list The input List, which can contain elements of any type
+     * @return A new List<String> containing only non-null String elements from the original List
+     */
     public static List<String> nullFilter(List<?> list) {
         return list.stream()
                 .filter(Objects::nonNull)
